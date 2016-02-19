66087c3620efafd5b3a9f9b80e0e2d4393ecda4e
asv0r <cdscott9@gmail.com>
2016-02-19 18:16:14 +0000

    Add seeds for places

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Place.create(name: 'Local Roots', address: '1314 Grandin Rd SW, Roanoke, VA 24015', description: 'After a long day of hiking this place was much needed. The food is amazing especially the flourless chocolate cake and the wait staff is top notch. There is no doubt that I would drive three hours to return for another great dining experience.')
+Place.create(name: 'The River and Rail', address: '2201 Crystal Spring Ave SW, Roanoke, VA 24014', description: 'What an incredible experience.  Great food, expertly crafted, and served with charm.  What more could you want? If you think a good value is getting fresh ingredients in unique flavor combinations, then this is the place for you. If you think value is getting the most amount of meat for a dollar - go someplace else.')
+Place.create(name: 'Wildflour Restaurant & Bakery', address: '1212 4th St SW, Roanoke, VA 24016', description: 'Great menu full of interesting twists on old favorites- feeling of fresh on all of it. And of course it is a bakery so no one leaves hungry unless they choose to go do. The location is historic too. First timers should think about Evie\'s red beads and rice if you can get all the other things out of your imagination.')
