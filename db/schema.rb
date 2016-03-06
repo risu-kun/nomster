44ffefb70de1c5dcc4b1ec905a2244e309ae7938
Risse Rigdon <risse.rigdon@gmail.com>
2016-03-06 21:29:38 +0000

    Add carrierwave gem and picture to Photo model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306195015) do
+ActiveRecord::Schema.define(version: 20160306212146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160306195015) do
     t.integer  "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "picture"
   end
 
   add_index "photos", ["place_id"], name: "index_photos_on_place_id", using: :btree
