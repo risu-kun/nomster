9597fac2a2665b985077e1bd5ffcc893dadb4b2a
Risse Rigdon <risse.rigdon@gmail.com>
2016-03-10 01:56:59 +0000

    Edit comment_add email to include place name and link

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,9 +1,12 @@
 class NotificationMailer < ApplicationMailer
 	default from: "no-reply@nomster-risukun.com"
 
-	def comment_added()
-		mail(to: "risse.rigdon@gmail.com",
-					subject: "A comment has been added to your place")
+	def comment_added(comment)
+		@place = comment.place
+		@place_owner = @place.user
+
+		mail(to: @place_owner.email,
+					subject: "A comment has been added to #{@place.name}")
 	end
 
 end
